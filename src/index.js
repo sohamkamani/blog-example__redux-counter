a3c2ebba63d59883e522034a7b7edff58bb1d064
sohamkamani <sohamkamani@gmail.com>
2016-02-23 18:05:22 +0530

    working counter

--- src/index.js
+++ src/index.js
@@ -1,4 +1,35 @@
+'use strict';
+import { createStore } from 'redux';
 
-document.write('My App...');
-console.log('Working...');
+const counter = (state = 0, action) => {
+  const { type } = action;
+  switch (type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
+const store = createStore(counter);
+
+const counterDisplay = document.getElementById('counter-display');
+const incrementButton = document.getElementById('counter-increment');
+const decrementButton = document.getElementById('counter-decrement');
+
+const render = () => {
+  counterDisplay.innerHTML = store.getState();
+};
+
+incrementButton.addEventListener('click', ()=>{
+  store.dispatch({type : 'INCREMENT'});
+});
+
+decrementButton.addEventListener('click', ()=>{
+  store.dispatch({type : 'DECREMENT'});
+});
+
+store.subscribe(render);
+render();
